ea7470a74d56774ebcc459b2359983ba71504f42
Lucas Azzola <lucas@azzola.dev>
2019-04-21 21:53:10 +1000

    Add tests for process substitution
    Failing due to parser bug

---
+++ tests/process_substitution/process_substitution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: Raise parser bug! (Unexpected 'OPEN_PAREN')
+# echo >(true)
+# echo <(true)
+# echo >(true) <(true)
+
+# wc <(cat /usr/share/dict/linux.words)
+
+# read -a list < <( od -Ad -w24 -t u2 /dev/urandom )
+
+exit
